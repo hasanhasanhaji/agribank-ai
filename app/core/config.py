45457510d8f685c4dd7e9c190d565c0fcfb74ac2
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-11 18:04:21 +0330

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -29,9 +29,11 @@ class Settings(BaseSettings):
         default="/api/v1",
         description="Base API prefix",)
 
-    database_url: str = Field( 
-        default=( "postgresql+asyncpg://" "postgres:0173276" "@localhost:5432/"
-                  "agribank" ), description="PostgreSQL database URL", )
+    database_url: str = Field(
+        ...,
+        description="PostgreSQL database URL",
+    )
+
 
     model_config = SettingsConfigDict(
         env_file= ".env",
