1f7f6a549e7b3ba03d5769dc8341c8133cd5119f
sorenagorgan <hasanhasanhaji@gmail.com>
2026-08-12 13:56:24 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,33 +1,61 @@
 from fastapi import FastAPI
+
 from app.api.health import router as health_router
 from app.core.config import get_settings
-from app.core.exceptions import AppException
-                                
+from app.core.exceptions import (
+    AppException,
+    app_exception_handler,
+)
 from app.core.loging import configure_logging
+from app.modules.customers.router import (
+    router as customer_router,
+)
 
-settings = get_settings()          # settings object
-configure_logging()                # setup logging
+
+settings = get_settings()
+
+configure_logging()
+
 
 app = FastAPI(
     title=settings.app_name,
     version=settings.app_version,
     description=(
         "AI-powered banking platform "
-          "for agricultural customers."
-          ), ) # create FastAPI app instance
+        "for agricultural customers."
+    ),
+)
 
 
-app.add_exception_handler( AppException, AppException.app_exception_handler, ) # exception handler for AppException
-app.include_router(health_router,
-                   prefix=settings.api_prefix,) # include health check router
+app.add_exception_handler(
+    AppException,
+    app_exception_handler,
+)
 
-# define root endpoint
-@app.get( "/", tags=["System"], ) 
-async def root() -> dict[str, str]: 
+
+app.include_router(
+    health_router,
+    prefix=settings.api_prefix,
+)
+
+
+app.include_router(
+    customer_router,
+    prefix=settings.api_prefix,
+)
+
+
+@app.get(
+    "/",
+    tags=["System"],
+)
+async def root() -> dict[str, str]:
     """
-    Root endpoint for the application.
-    Returns a simple message indicating that the application is running.
+    Return basic application information.
     """
-    return {"application": settings.app_name,
-             "version": settings.app_version,
-               "environment": settings.environment,}
+
+    return {
+        "application": settings.app_name,
+        "version": settings.app_version,
+        "environment": settings.environment,
+    }
